81a33a9ac42ff326be4fca9673b6d43735867fae
ajdivotf <eckyl@bk.ru>
2023-04-16 12:40:48 +0300

    do stars

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,21 @@
 import { Inter } from 'next/font/google';
-import { Button, Htag, P, Tag } from '@component/components';
+import { Button, Htag, P, Rating, Tag } from '@component/components';
 import { useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(): JSX.Element {
-  const [counter, setCounter] = useState<number>(0);
-  useEffect(() => {
-    console.log('Counter ' + counter);
-    return function cleanup() {
-      console.log('Unmount');
-    };
-  });
-  useEffect(() => {
-    console.log('Mounted');
-  }, []);
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag='h1'>{counter}</Htag>
-      <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Count++</Button>
+      <Htag tag='h1'>Заголовок</Htag>
+      <Button appearance='primary' arrow='right'>Count++</Button>
       <Button appearance='ghost' arrow='down'>Кнопка1</Button>
       <P>Средний!</P>
       <Tag size='s' color='primary'>Ghooost</Tag>
       <Tag size='s' color='primary'>Ghooost</Tag>
       <Tag size='s' color='primary'>Ghooost</Tag>
-      <Tag size='s' color='primary'>Ghooost</Tag>
+      <Rating rating={rating} isEditable={true} setRating={setRating} />
     </>
   )
 };
