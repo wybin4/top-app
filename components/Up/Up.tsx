18e3754aac6760f8b3a785acf7d8c04230784e0d
ajdivotf <eckyl@bk.ru>
2023-04-20 19:40:51 +0300

    add scrollTop

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,15 +1,29 @@
-import { PProps } from './P.props'
-import styles from './P.module.css';
+import styles from './Up.module.css';
 import cn from 'classnames';
+import UpIcon from './up.svg';
+import { useScrollY } from '@component/hooks/useScrollY';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
 
-export const P = ({ size = 'm', children, className, ...props }: PProps): JSX.Element => {
-	return (<p className={cn(styles.p, className, {
-		[styles.s]: size == 's',
-		[styles.m]: size == 'm',
-		[styles.l]: size == 'l',
-	})}
-		{...props}
+export const Up = (): JSX.Element => {
+	const controls = useAnimation();
+	const y = useScrollY();
+
+	useEffect(() => {
+		controls.start({ opacity: y / document.body.scrollHeight });
+	}, [y, controls]);
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	};
+	return (<motion.button
+		className={styles.up}
+		onClick={scrollToTop}
+		animate={controls}
+		initial={{ opacity: 0 }}
 	>
-		{children}
-	</p>)
+		<UpIcon />
+	</motion.button>)
 }
--- components/index.ts
+++ components/index.ts
@@ -11,3 +11,4 @@ export * from './Search/Search';
 export * from './Product/Product';
 export * from './Divider/Divider';
 export * from './Review/Review';
+export * from './Up/Up';
