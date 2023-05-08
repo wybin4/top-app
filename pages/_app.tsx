6bd3ad1a4ed85fcdcecdbec6cee55d72a4183489
ajdivotf <eckyl@bk.ru>
2023-05-08 17:12:24 +0300

    fix errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,10 @@ import Head from 'next/head';
 import React from 'react';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from 'react-yandex-metrika';
+import Router from 'next/router';
 
 export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
-  router.events.on('routeChangeComplete', (url: string) => {
+  Router.events.on('routeChangeComplete', (url: string) => {
     if (typeof window !== 'undefined') {
       ym('hit', url);
     }
